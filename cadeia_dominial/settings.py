f5a0c514f58a4137ba1fb9dc75c55bf691a6398e
epassarojr <epassarojr@gmail.com>
2025-06-09 11:31:33 -0300

    starting..

--- cadeia_dominial/settings.py
+++ cadeia_dominial/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cadeia_dominial',
+    'django_extensions',
+    'django.contrib.gis',
+    'dominial',
 ]
 
 MIDDLEWARE = [
