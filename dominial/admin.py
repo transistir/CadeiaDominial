7405b02a5b5cf8be4ecec863bf22ba4eb6df2bc2
epassarojr <epassarojr@gmail.com>
2025-06-10 08:17:10 -0300

    title names on admin page

--- dominial/admin.py
+++ dominial/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import TIs, Cartorios, Pessoas, Imovel, Alteracoes
+
+admin.site.site_header = "Sistema de Cadeia Dominial"
+admin.site.site_title = "Administração"
+admin.site.index_title = "Painel de Gestão"
+
+
 # Register your models here.
 
 admin.site.register(TIs)
