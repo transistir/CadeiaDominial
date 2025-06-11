8819087a480c6978d9139f1f24bcf22fd83f823a
epassarojr <epassarojr@gmail.com>
2025-06-11 17:01:36 -0300

    primeira view, lista de imoveis

--- cadeia_dominial/urls.py
+++ cadeia_dominial/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from dominial.views import home
+from dominial.views import home, imoveis
 
 urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login', http_method_names=['get', 'post']), name='logout'),
+    path('imoveis/', imoveis, name='imoveis'),
 ]
--- dominial/views.py
+++ dominial/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Imovel
 # Create your views here.
 
 @login_required
 def home(request):
     return render(request, 'dominial/home.html')
+
+def imoveis(request):
+    imoveis = Imovel.objects.all() #.order_by('-data_cadastro')[:5] 
+    return render(request, 'dominial/imoveis.html', {'imoveis': imoveis})
