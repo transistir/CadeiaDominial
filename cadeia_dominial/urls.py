df095df6dd6675d039cbcf386ec61350ebcd3b85
hiurequeiroz <hiure@riseup.net>
2025-06-12 18:14:13 -0300

    refactor: centraliza rotas do app dominial usando include em urls.py do projeto principal

--- cadeia_dominial/urls.py
+++ cadeia_dominial/urls.py
@@ -15,27 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from dominial.views import (
-    home, tis_form, tis_detail, tis_delete,
-    imovel_form, imovel_edit, imovel_delete,
-    imoveis, cartorios, pessoas, alteracoes
-)
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('tis/cadastro/', tis_form, name='tis_form'),
-    path('tis/<int:tis_id>/', tis_detail, name='tis_detail'),
-    path('tis/<int:tis_id>/excluir/', tis_delete, name='tis_delete'),
-    path('tis/<int:tis_id>/imovel/cadastro/', imovel_form, name='imovel_form'),
-    path('tis/<int:tis_id>/imovel/<int:imovel_id>/editar/', imovel_edit, name='imovel_edit'),
-    path('tis/<int:tis_id>/imovel/<int:imovel_id>/excluir/', imovel_delete, name='imovel_delete'),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login', http_method_names=['get', 'post']), name='logout'),
-    path('imoveis/', imoveis, name='imoveis'),
-    path('cartorios/', cartorios, name='cartorios'),
-    path('pessoas/', pessoas, name='pessoas'),
-    path('alteracoes/', alteracoes, name='alteracoes'),
+    path('', include('dominial.urls')),  # Incluir todas as URLs de dominial
 ]
