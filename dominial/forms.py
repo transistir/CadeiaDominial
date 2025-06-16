90b29ab8c6f66b2d083c36b378e70dc9717d280d
hiurequeiroz <hiure@riseup.net>
2025-06-16 13:56:11 -0300

    feat(forms): adiciona AlteracaoForm para registro de alterações de imóvel - Permite criar e editar alterações vinculadas ao imóvel - Inclui campos completos e widgets estilizados

--- dominial/forms.py
+++ dominial/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import TIs, Imovel, Pessoas, TerraIndigenaReferencia, Cartorios
+from .models import TIs, Imovel, Pessoas, TerraIndigenaReferencia, Cartorios, Alteracoes
 
 class TIsForm(forms.ModelForm):
     terra_referencia = forms.ModelChoiceField(
@@ -60,5 +60,34 @@ class ImovelForm(forms.ModelForm):
             'sncr': forms.TextInput(attrs={'class': 'form-control'}),
             'sigef': forms.TextInput(attrs={'class': 'form-control'}),
             'descricao': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'observacoes': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
+
+class AlteracaoForm(forms.ModelForm):
+    class Meta:
+        model = Alteracoes
+        fields = [
+            'tipo_alteracao_id', 'cartorio', 'livro', 'folha', 'data_alteracao',
+            'registro_tipo', 'averbacao_tipo', 'titulo', 'cartorio_origem',
+            'livro_origem', 'folha_origem', 'data_origem', 'transmitente',
+            'adquirente', 'valor_transacao', 'area', 'observacoes'
+        ]
+        widgets = {
+            'tipo_alteracao_id': forms.Select(attrs={'class': 'form-control'}),
+            'cartorio': forms.Select(attrs={'class': 'form-control'}),
+            'livro': forms.TextInput(attrs={'class': 'form-control'}),
+            'folha': forms.TextInput(attrs={'class': 'form-control'}),
+            'data_alteracao': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'registro_tipo': forms.Select(attrs={'class': 'form-control'}),
+            'averbacao_tipo': forms.Select(attrs={'class': 'form-control'}),
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'cartorio_origem': forms.Select(attrs={'class': 'form-control'}),
+            'livro_origem': forms.TextInput(attrs={'class': 'form-control'}),
+            'folha_origem': forms.TextInput(attrs={'class': 'form-control'}),
+            'data_origem': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'transmitente': forms.Select(attrs={'class': 'form-control'}),
+            'adquirente': forms.Select(attrs={'class': 'form-control'}),
+            'valor_transacao': forms.NumberInput(attrs={'class': 'form-control'}),
+            'area': forms.NumberInput(attrs={'class': 'form-control'}),
             'observacoes': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
         } 
