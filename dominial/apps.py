c7776839c3cd456d0f188fcf826aacd3c5f90a98
hiurequeiroz <hiure@riseup.net>
2025-09-21 00:38:33 -0300

    feat: Adicionar signal Django para processar origens automaticamente- Criar signal post_save para modelo Lancamento- Processar origens automaticamente sempre que lançamento for salvo - Registrar signal no apps.py- Resolve problema de documentos não criados automaticamente quando lançamentos são inseridos diretamente no banco

--- dominial/apps.py
+++ dominial/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DominialConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dominial'
+    
+    def ready(self):
+        import dominial.signals
